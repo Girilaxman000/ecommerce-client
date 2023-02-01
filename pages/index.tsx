33c6ae445eac883629544c5e82e9263f45e7a50b
Girilaxman000 <kesharigiri000@gmail.com>
2023-02-01 14:57:10 +0545

    fix some issue

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,7 @@ const inter = Inter({ subsets: ["latin"] });
 export default function Home() {
   return (
     <>
-      <div className="btn-blue">Hello Geeks!</div>
+      <div className="hello-world">Hello World!</div>
     </>
   );
 }
