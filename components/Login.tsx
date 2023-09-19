6ecd7c226a7613256c4e9e28f950924a257b5d4c
Laxman Giri <kesharigiri000@gmail.com>
2023-09-19 17:26:39 +0545

    add pages

--- components/Login.tsx
+++ components/Login.tsx
@@ -1,10 +1,22 @@
+/* eslint-disable react/no-unescaped-entities */
 import Link from "next/link";
-import { LockClosedIcon, UserCircleIcon } from "@heroicons/react/24/solid";
+import {
+  BeakerIcon,
+  LockClosedIcon,
+  UserCircleIcon,
+} from "@heroicons/react/24/solid";
+import { useState } from "react";
 import * as Yup from "yup";
 import { useFormik } from "formik";
+import Image from "next/image";
+import { useMutation } from "react-query";
 import axios from "axios";
 
-const Register = () => {
+export default function Login() {
+  const Login = (data: any) => {
+    return axios.post("http://localhost:8000/sign_in", data);
+  };
+  const { mutate: SignIn } = useMutation(Login);
   const initialValues = {
     username: "",
     password: "",
@@ -30,21 +42,14 @@ const Register = () => {
     useFormik({
       initialValues: initialValues,
       onSubmit: (values) => {
-        axios
-          .post("http://localhost:8000/sign_up", {
-            Email: values.username,
-            Password: values.password,
-          })
-          .then()
-          .catch();
+        SignIn(values);
       },
       validationSchema: validationSchema,
     });
-
   return (
     <div className="h-[100vh] flex justify-center items-center bg-gradient-to-r from-[#fbc2eb] to-[#a6c1ee]">
       <div className="bg-white p-8 rounded-xl w-[400px]">
-        <h1 className="text-center text-3xl font-bold ">Register</h1>
+        <h1 className="text-center text-3xl font-bold ">Login</h1>
 
         <form className="mt-3 flex flex-col gap-3" onSubmit={handleSubmit}>
           <div className="gap-5">
@@ -75,13 +80,13 @@ const Register = () => {
                 className={`w-full py-2 pr-2 pl-8 border-b-2 ${
                   errors.username && touched.username ? "border-red-500" : ""
                 } border-gray-300 focus:outline-none focus:
-                focus:
-                    ${
-                      errors.username && touched.username
-                        ? "border-red-500"
-                        : "border-indigo-500 "
-                    }
-                `}
+              focus:
+                  ${
+                    errors.username && touched.username
+                      ? "border-red-500"
+                      : "border-indigo-500 "
+                  }
+              `}
               />
               {errors.username && touched.username && (
                 <span className="text-red-500 text-sm mt-1">
@@ -116,19 +121,15 @@ const Register = () => {
                 }}
                 placeholder="Enter Name"
                 className={`w-full py-2  pr-2 border-b-2 pl-8 
-                    ${
-                      errors.password && touched.password
-                        ? "border-red-500"
-                        : ""
-                    }
-                    border-gray-300 focus:outline-none  focus:
-                    ${
-                      errors.password && touched.password
-                        ? "border-red-500"
-                        : "border-indigo-500 "
-                    }
-                    
-                     `}
+                  ${errors.password && touched.password ? "border-red-500" : ""}
+                  border-gray-300 focus:outline-none  focus:
+                  ${
+                    errors.password && touched.password
+                      ? "border-red-500"
+                      : "border-indigo-500 "
+                  }
+                  
+                   `}
               />
               {errors.password && touched.password && (
                 <span className="text-red-500 text-sm mt-1">
@@ -136,23 +137,40 @@ const Register = () => {
                 </span>
               )}
             </div>
+            <Link href="/change-password" className=" text-right mt-6">
+              Forgot Password ?
+            </Link>
           </div>
+
           <button
             type="submit"
             className="w-full p-2 bg-gradient-to-r from-[#fbc2eb] to-[#a6c1ee] rounded-3xl"
           >
-            Register
+            Login
           </button>
         </form>
+        <p className="text-center mt-2">Or SignUp Using</p>
+        <div className="flex justify-center mt-2 gap-1">
+          <Image
+            src={"/assets/facebook.svg"}
+            alt="facebook"
+            width={25}
+            height={25}
+          />
+          <Image
+            src={"/assets/google.svg"}
+            alt="google"
+            width={25}
+            height={25}
+          />
+        </div>
         <p className="mt-5">
-          Already registered ?{" "}
-          <Link className="text-blue-600" href="/">
-            Login
+          Haven't registered yet?{" "}
+          <Link className="text-blue-600" href="/register">
+            Register
           </Link>
         </p>
       </div>
     </div>
   );
-};
-
-export default Register;
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,5 @@
-import { Inter } from "@next/font/google";
-
-const inter = Inter({ subsets: ["latin"] });
+import Login from "../components/Login";
 
 export default function Home() {
-  return (
-    <>
-      <div className="hello-world">
-        1. User Register 2. User Login (can change password) 3. User receives
-        jwt token and needs to pass as bearer token for authorization
-      </div>
-    </>
-  );
+  return <Login />;
 }
